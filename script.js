b2e5a473abfbfae88e0341e5e4ef283fbf5bdf65
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2023-09-25 20:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const levelElement = document.getElementById("level");
+let level = 1;
+while (true) {
+	if (levelElement === "BODY"){
+		level++;
+		break;
+	}
+	levelElement = levelElement.parentNode;
+	level++;
+}
+
+alert(`The level of the element is: ${level}`);
